85b603689b703883969b5e7518dbe1c3212766a0
Weiqun Zhang <weiqunzhang@lbl.gov>
2019-03-13 15:38:43 -0700

    version in WarpX

--- src/WarpXParser.cpp
+++ src/WarpXParser.cpp
@@ -1,9 +1,18 @@
 
 #include "WarpXParser.H"
 
-WarpXParser::WarpXParser (std::string func_body)
-    : expression(std::move(func_body))
+WarpXParser::WarpXParser (std::string const& func_body)
 {
+    define(func_body);
+}
+
+void
+WarpXParser::define (std::string const& func_body)
+{
+    clear();
+
+    m_expression = func_body;
+
 #ifdef _OPENMP
     int nthreads = omp_get_max_threads();
 #else
@@ -11,7 +20,7 @@ WarpXParser::WarpXParser (std::string func_body)
 #endif
     m_parser.resize(nthreads);
 
-    std::string f = expression + "\n";
+    std::string f = m_expression + "\n";
     m_parser[0] = wp_c_parser_new(f.c_str());
 
 #ifdef _OPENMP
@@ -23,21 +32,31 @@ WarpXParser::WarpXParser (std::string func_body)
         }
     }
 #endif
-
-    p0 = m_parser[0];
 }
 
 WarpXParser::~WarpXParser ()
 {
+    clear();
+}
+
+void
+WarpXParser::clear ()
+{
+    if (!m_parser.empty())
+    {
 #ifdef _OPENMP
 #pragma omp parallel
-    {
-        int tid = omp_get_thread_num();
-        wp_parser_delete(m_parser[tid]);
-    }
+        {
+            int tid = omp_get_thread_num();
+            wp_parser_delete(m_parser[tid]);
+        }
 #else
-    wp_parser_delete(p0);
+        wp_parser_delete(m_parser[0]);
 #endif
+    }
+    m_expression.clear();
+    m_parser.clear();
+    m_variables.clear();
 }
 
 void
@@ -45,10 +64,33 @@ WarpXParser::registerVariable (std::string const& name, double& var)
 {
     // We assume this is called inside OMP parallel region
 #ifdef _OPENMP
-    wp_parser_regvar(m_parser[omp_get_thread_num()], name.c_str(), &var);
+    const int tid = omp_get_thread_num();
 #else
-    wp_parser_regvar(p0, name.c_str(), &var);
+    const int tid = 0;
 #endif
+    wp_parser_regvar(m_parser[tid], name.c_str(), &var);
+}
+
+// This must be called outside OpenMP parallel region.
+void
+WarpXParser::registerVariables (std::vector<std::string> const& names)
+{
+#ifdef _OPENMP
+    const int nthreads = omp_get_max_threads();
+#else
+    const int nthreads = 1;
+#endif
+    m_variables.resize(nthreads);
+    const int nnames = names.size();
+#ifdef _OPENMP
+#pragma omp parallel for
+#endif
+    for (int i = 0; i < nthreads; ++i) {
+        m_variables[i].resize(nnames);
+        for (int j = 0; j < nnames; ++i) {
+            wp_parser_regvar(m_parser[i], names[j].c_str(), &(m_variables[i][j]));
+        }
+    }
 }
 
 void
@@ -62,7 +104,7 @@ WarpXParser::setConstant (std::string const& name, double c)
         wp_parser_setconst(m_parser[omp_get_thread_num()], name.c_str(), c);
     }
 #else
-    wp_parser_setconst(p0, name.c_str(), c);
+    wp_parser_setconst(m_parser[0], name.c_str(), c);
 #endif
 }
 
@@ -73,12 +115,12 @@ WarpXParser::print () const
 #pragma omp critical(warpx_parser_pint)
     wp_ast_print(m_parser[omp_get_thread_num()]->ast);
 #else
-    wp_ast_print(p0->ast);
+    wp_ast_print(m_parser[0]->ast);
 #endif
 }
 
 std::string const&
 WarpXParser::expr () const
 {
-    return expression;
+    return m_expression;
 }
